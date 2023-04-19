49c9a24d8b89eb077c43971159a2f6a576d38949
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2023-04-19 12:30:01 -0700

    Update
    Modificacion de endpoints Get ResultadosParametros y Put ResultadosParametros

--- src/controller/resultados-parametros-controller.js
+++ src/controller/resultados-parametros-controller.js
@@ -1,97 +1,120 @@
-const oMySQLConnection = require('../database');
+const oMySQLConnection = require("../database");
 
 // GETS
 const getResultadosParametrosByResultadoId = (req, res) => {
-    const oResultadoId = req.params["resultId"];
+  const oResultadoId = req.params["resultId"];
 
-    const query = "CALL GetResultadosParametrosByResultadoIdSP(?);";
+  const query = "CALL GetAnalisissByResultadoIdSP(?);";
 
-    oMySQLConnection.query(query, [oResultadoId], (err, rows, fields) => {
-        if (!err) {
-            res.json(rows);
-        } else {
-            res.status(400).send("Bad request.")
-        }
-    });
-}
+  oMySQLConnection.query(query, [oResultadoId], (err, rows, fields) => {
+    if (!err) {
+      res.json(rows);
+    } else {
+      res.status(400).send("Bad request.");
+    }
+  });
+};
 
 const getResultadosParametrosByParameterId = (req, res) => {
-    const oParametroId = req.params["parameterId"];
+  const oParametroId = req.params["parameterId"];
 
-    const query = 'CALL GetResultadosParametroByParameterIdSP(?);';
-    oMySQLConnection.query(query, [oParametroId], (err, rows) => {
-        if (!err) {
-            res.json(rows);
-        } else {
-            res.status(400).send("Bad request.");
-        }
-    });
-}
+  const query = "CALL GetResultadosParametroByParameterIdSP(?);";
+  oMySQLConnection.query(query, [oParametroId], (err, rows) => {
+    if (!err) {
+      res.json(rows);
+    } else {
+      res.status(400).send("Bad request.");
+    }
+  });
+};
 
 const getResultadosParametrosByFecha = (req, res) => {
-    const { oParametrosFecha } = req.body;
+  const { oParametrosFecha } = req.body;
 
-    const query = 'CALL GetParametrosByFechaSP(?);';
-    oMySQLConnection.query(query, [oParametrosFecha], (err, rows) => {
-        if (!err) {
-        res.json(rows);
-        } else {
-        res.status(400).send("Bad request.");
-        }
-    });
-  }
+  const query = "CALL GetParametrosByFechaSP(?);";
+  oMySQLConnection.query(query, [oParametrosFecha], (err, rows) => {
+    if (!err) {
+      res.json(rows);
+    } else {
+      res.status(400).send("Bad request.");
+    }
+  });
+};
 
-  //CREATES
+//CREATES
 
-  const createResultadoParametro = (req, res) => {
-    const {oParametroId, oResultadoId, oValor} = req.body;
+const createResultadoParametro = (req, res) => {
+  const { oParametroId, oResultadoId, oValor } = req.body;
 
-    const query = 'CALL insertResultParameter(?, ?, ?);';
+  const query = "CALL insertResultParameter(?, ?, ?);";
 
-    oMySQLConnection.query(query, [oParametroId, oResultadoId, oValor], (err, rows) => {
-        if (!err) {
-            res.status(200).send("The value was created successfully.");
-        } else {
-            res.status(400).send("One of the parameters introduced is wrong.");
-        }
-    });
-  }
+  oMySQLConnection.query(
+    query,
+    [oParametroId, oResultadoId, oValor],
+    (err, rows) => {
+      if (!err) {
+        res.status(200).send("The value was created successfully.");
+      } else {
+        res.status(400).send("One of the parameters introduced is wrong.");
+      }
+    }
+  );
+};
 
-
-  //UPDATES
+//UPDATES
 const updateResultadoParametro = (req, res) => {
-    const { oParametroId, oResultadoId, oValor } = req.body;
+  const { oParametroId, oResultadoId, oValor } = req.body;
 
-    const query = 'CALL UpdateResultadoParametroSP(?, ?, ?);';
-    oMySQLConnection.query(query, [oParametroId, oResultadoId, oValor], (err, rows) => {
-        if (!err) {
-            res.status(200).send("The value was updated successfully.");
-        } else {
-            res.status(400).send("One of the parameters introduced is wrong.");
-        }
-    });
-}
+  const query = "CALL UpdateResultadoParametroSP(?, ?, ?);";
+  oMySQLConnection.query(
+    query,
+    [oParametroId, oResultadoId, oValor],
+    (err, rows) => {
+      if (!err) {
+        res.status(200).send("The value was updated successfully.");
+      } else {
+        res.status(400).send("One of the parameters introduced is wrong.");
+      }
+    }
+  );
+};
+const updateResultadoParametroValue = (req, res) => {
+  const { oResultadoParametroID, oValor } = req.body;
 
+  const query = "CALL UpdateResultadoParametroValueSP(?, ?);";
+  oMySQLConnection.query(
+    query,
+    [oResultadoParametroID, oValor],
+    (err, rows) => {
+      if (!err) {
+        res.status(200).send("The value was updated successfully.");
+      } else {
+        res.status(400).send("One of the parameters introduced is wrong.");
+      }
+    }
+  );
+};
 //DELETES
 const deleteResultadoParametro = (req, res) => {
-    const { oParametroId, oResultadoId } = req.body;
+  const { oParametroId, oResultadoId } = req.body;
 
-    const query = 'CALL DeleteResultadoParametroSP(?, ?);';
-    oMySQLConnection.query(query, [oParametroId, oResultadoId], (err, rows) => {
-        if (!err) {
-            res.json(rows);
-        } else {
-            res.status(400).send("One of the parameters introduced is wrong.");
-        }
-    })
-}
+  const query = "CALL DeleteResultadoParametroSP(?, ?);";
+  oMySQLConnection.query(query, [oParametroId, oResultadoId], (err, rows) => {
+    if (!err) {
+      res.json(rows);
+    } else {
+      res.status(400).send("One of the parameters introduced is wrong.");
+    }
+  });
+};
 
 //EXPORTS
 module.exports = {
-    getResultadosParametrosByResultadoId,
-    getResultadosParametrosByParameterId,
-    getResultadosParametrosByFecha,
-    createResultadoParametro,
-    updateResultadoParametro,
-    deleteResultadoParametro
+  getResultadosParametrosByResultadoId,
+  getResultadosParametrosByParameterId,
+  getResultadosParametrosByFecha,
+  createResultadoParametro,
+  updateResultadoParametro,
+  updateResultadoParametroValue,
+  deleteResultadoParametro,
 };
--- src/routes/resultados-parametros.js
+++ src/routes/resultados-parametros.js
@@ -1,22 +1,35 @@
 const express = require("express");
-const resultadosParametrosController = require('../controller/resultados-parametros-controller'); 
+const resultadosParametrosController = require("../controller/resultados-parametros-controller");
 const router = express.Router();
 
-
 //GET   /resultados-parametros/all/:resultId
-router.get("/all/:resultId", resultadosParametrosController.getResultadosParametrosByResultadoId);
+router.get(
+  "/all/:resultId",
+  resultadosParametrosController.getResultadosParametrosByResultadoId
+);
 
 //GET   /resultados-parametros/:parameterId
-router.get("/:parameterId", resultadosParametrosController.getResultadosParametrosByParameterId);
+router.get(
+  "/:parameterId",
+  resultadosParametrosController.getResultadosParametrosByParameterId
+);
 
 //POST /resultados-parametros
 router.post("/", resultadosParametrosController.createResultadoParametro);
 
 //POST  /resultados-parametros/date
-router.post("/date", resultadosParametrosController.getResultadosParametrosByFecha);
+router.post(
+  "/date",
+  resultadosParametrosController.getResultadosParametrosByFecha
+);
 
 //PUT   /resultados-parametros
 router.put("/", resultadosParametrosController.updateResultadoParametro);
+//PUT /resultados-parametros/byID
+router.put(
+  "/byID",
+  resultadosParametrosController.updateResultadoParametroValue
+);
 
 //PUT    /resultados-parametros/delete
 router.put("/delete", resultadosParametrosController.deleteResultadoParametro);
