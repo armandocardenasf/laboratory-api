d2bb6b1bacd4ac66856c6bd537fdfaec2203f3e3
user <email@example.com>
2023-04-27 12:49:08 -0700

    sign and create middleware

---
+++ src/helpers/adminAuth.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET;
+
+module.exports = (req, res, next) => {
+  const authHeader = req.header("authorization");
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token === null) res.status(400).send();
+
+  jwt.verify(token, TOKEN_SECRET, (err, data) => {
+    const userType = data.type;
+
+    if (err || userType !== "admin") {
+      res.send(403).send();
+    } else {
+      next();
+    }
+  });
+};
---
+++ src/helpers/tokens.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET;
+
+const generateToken = (name, type) => {
+  const data = {
+    name: name,
+    type: type,
+  };
+
+  return jwt.sign(data, TOKEN_SECRET, { expiresIn: "14d" });
+};
