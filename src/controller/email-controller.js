9a0b20f1235a94ad2e2b0c724429178b3425b289
oscark22 <98506040+oscark22@users.noreply.github.com>
2023-05-10 22:57:18 -0700

    changes

--- src/controller/email-controller.js
+++ src/controller/email-controller.js
@@ -5,6 +5,19 @@ const transporter = require("../helpers/email-transporter");
 const sendEmail = (req, res) => {
   const { oIdRecepcion } = req.body;
 
+  //
+  // TODO: validate Recepcion current state to determine if the email can be sent or not.
+  //
+
+  // get the user's email.
+  let query = "CALL GetRecepcionExternoEmailSP(?);";
+  oMySQLConnection.query(query, [oIdRecepcion], (err, rows) => {
+    if (err) {
+      res.status(500).send("An error occurred. Email not found.");
+      return;
+    }
+  });
+
   const mailOptions = {
     from: "lab.cevit@cetys.mx",
     to: "oscar.encinas@cetys.edu.mx",
@@ -12,14 +25,16 @@ const sendEmail = (req, res) => {
     text: "Esta es una prueba usando nodemailer.",
   };
 
+  // send the email.
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      res.status(500).send("The email couldn't be sent.");
+      res.status(500).send("An error occurred while sending the email.");
       return;
     }
   });
 
-  const query = "CALL UpdateSendEmailSP(?);";
+  // update email state to sent.
+  query = "CALL UpdateSendEmailSP(?);";
   oMySQLConnection.query(query, [oIdRecepcion], (err, rows) => {
     if (err) {
       res.status(500).send("The email couldn't be sent.");
