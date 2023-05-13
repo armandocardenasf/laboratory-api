d5c5bc54833f20378d3f6a537159513414a632bb
oscark22 <98506040+oscark22@users.noreply.github.com>
2023-05-13 13:47:13 -0700

    formatting

--- src/controller/pdf-controller.js
+++ src/controller/pdf-controller.js
@@ -7,10 +7,7 @@ require("jspdf-autotable");
 const sendPDF = async (req, res) => {
   const { oResultadoId } = req.body;
 
-  // get the data. Name of the parameter and also its value for each one of them.
-  // need to get it from each resultado.
-  // create store procedure.
-
+  // get the parameters from db.
   const query = "CALL GetResultadoParameterValueSP(?);";
   const [rows, fields] = await oMySQLConnection
     .promise()
@@ -21,46 +18,9 @@ const sendPDF = async (req, res) => {
     return;
   }
 
-  const groupedData = rows[0].reduce((acc, curr) => {
-    const key = curr.nombre;
-    const unit = curr.unidades;
-    const existing = acc.find((item) => item.key === key);
-    if (existing) {
-      existing.values.push(curr.valor);
-    } else {
-      acc.push({ key, unit, values: [curr.valor] });
-    }
-    return acc;
-  }, []);
-
-  const formattedData = [];
-
-  for (let i = 0; i < groupedData[0].values.length; i++) {
-    for (let j = 0; j < groupedData.length; j++) {
-      const { key, unit, values } = groupedData[j];
-
-      // removing "áéíóú"
-      const normalizedKey = key
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "");
-      const normalizedValue = values[i]
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "");
-
-      formattedData.push({
-        parametro: normalizedKey,
-        valor: normalizedValue,
-        unidad: unit,
-        metodologia: "metodologia",
-        desviacion_estandar: "desviacion estandar",
-        incertidumbre: "incertidumbre",
-        desviacion_metodo: "desviacion metodo",
-      });
-    }
-  }
-
-  console.log(formattedData);
-  const doc = new PdfFormat().generate(formattedData);
+  const groupedData = PdfFormat.getGroupedData(rows[0]);
+  const formattedData = PdfFormat.getFormattedData(groupedData);
+  const doc = PdfFormat.generate(formattedData);
 
   // send pdf
   const buffer = doc.output();
--- src/helpers/pdf-format.js
+++ src/helpers/pdf-format.js
@@ -2,7 +2,50 @@ const { jsPDF } = require("jspdf");
 require("jspdf-autotable");
 
 class PdfFormat {
-  generate(data) {
+  static getFormattedData(groupedData) {
+    const formattedData = [];
+    for (let i = 0; i < groupedData[0].values.length; i++) {
+      for (let j = 0; j < groupedData.length; j++) {
+        const { key, unit, values } = groupedData[j];
+
+        // removing "áéíóú"
+        const normalizedKey = key
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "");
+        const normalizedValue = values[i]
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "");
+
+        formattedData.push({
+          parametro: normalizedKey,
+          valor: normalizedValue,
+          unidad: unit,
+          metodologia: "metodologia",
+          desviacion_estandar: "desviacion estandar",
+          incertidumbre: "incertidumbre",
+          desviacion_metodo: "desviacion metodo",
+        });
+      }
+    }
+
+    return formattedData;
+  }
+
+  static getGroupedData(data) {
+    return data.reduce((acc, curr) => {
+      const key = curr.nombre;
+      const unit = curr.unidades;
+      const existing = acc.find((item) => item.key === key);
+      if (existing) {
+        existing.values.push(curr.valor);
+      } else {
+        acc.push({ key, unit, values: [curr.valor] });
+      }
+      return acc;
+    }, []);
+  }
+
+  static generate(data) {
     const doc = new jsPDF();
 
     let info = [];
